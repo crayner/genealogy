efcd9c08fad35428195fad66a9b25fccc35f7319
Craig Rayner <craig@craigrayner.com>
2022-05-23 12:47:09 +1000

    Look for double space in death location age

--- src/Manager/WikitreeParser.php
+++ src/Manager/WikitreeParser.php
@@ -436,7 +436,7 @@ class WikitreeParser
             if (str_contains($result['death']['location'], 'about')) $result['death']['about'] = true;
         }
         $result['death']['location'] = trim(str_replace(['[uncertain]',"\r","\n",'Died','before','after','about','[death date?]', $xxx],'', $result['death']['location']));
-        $result['death']['location'] = preg_replace("/at age (\d.)/","", $result['death']['location']);
+        $result['death']['location'] = preg_replace("/at( .)age (\d.)/","", $result['death']['location']);
         $result['death']['location'] = preg_replace('/^in /',"", $result['death']['location']);
 
         $result['age']['status'] = false;
