048100a3071af08bae4d6a2c3cc96a756f901fd5
Craig Rayner <crayner@users.noreply.github.com>
2023-04-08 16:01:20 +1000

    Added Coordinate Links. Added Alternate Names.
    Signed-off-by: Craig Rayner <crayner@users.noreply.github.com>

--- assets/category/AlternateNames.js
+++ assets/category/AlternateNames.js
@@ -1,9 +1,7 @@
 'use strict';
 
-import React, {Fragment} from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
-import * as Style from "../component/StyledCSS";
-import OpenFormSection from "./OpenFormSection";
 import {DarkGreenBold, DarkGreenP} from "../component/StyledCSS";
 
 export default function AlternateNames(props) {
